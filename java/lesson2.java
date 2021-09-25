c0114a59edda6b60f2fc6b6f7f30c4b58d0c632b
cjqxss123 <91357087+cjqxss123@users.noreply.github.com>
2021-09-24 23:50:04 -0400

    Create lesson2.java

---
+++ java/lesson2.java
@@ -0,0 +1,19 @@
+package java;
+
+public class lesson2 {
+	// variable is given to a identifier such as age height and the like
+	// types are int double String;
+	public static void main(String[] args) {
+		String name = "toyota";// given the name equals to toyed
+		double engine = 4.7; // given the engine with a double number 4.7
+		int year = 2019;// given the car made year 2019
+		System.out.println(name);
+		System.out.println(engine);
+		System.out.println(year);
+		String name1 = "jhon";
+		int age = 21;//int 
+		boolean isGraduate = false;//boolean either true or false
+		double averageScore = 4.3;
+		System.out.println(name1 + age + isGraduate + averageScore);
+	}
+}
