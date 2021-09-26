d74f4f71545b2aeab87713b6178f76f81fa3b2e0
cjqxss123 <91357087+cjqxss123@users.noreply.github.com>
2021-09-26 04:08:47 -0400

    Create lesson3_variable.java

---
+++ java/lesson3_variable.java
@@ -0,0 +1,17 @@
+package learningcoding;
+
+import java.util.Scanner;
+
+public class lesson3varible {
+	public static void main(String[] args) {
+		Scanner myCar = new Scanner(System.in);
+		// assigned a variable manually;
+		/*
+		 * Read a byte - nextByte() Read a short - nextShort() Read an int - nextInt()
+		 * Read a long - nextLong() Read a float - nextFloat() Read a double -
+		 * nextDouble() Read a boolean - nextBoolean() Read a complete line - nextLine()
+		 * Read a word - next()
+		 */
+		System.out.println(myCar.nextInt());
+	}
+}
